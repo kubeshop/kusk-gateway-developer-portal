934401b7ace4377baa2608fb115754e506bef148
Mahmoud Mortada <mortda.m.11@gmail.com>
2022-11-17 12:47:12 +0200

    fix: coloring

--- src/components/OpenapiDoc/OpenapiDoc.tsx
+++ src/components/OpenapiDoc/OpenapiDoc.tsx
@@ -3,10 +3,10 @@ import KuskLogo from '@assets/KuskLogo.svg';
 
 import 'rapidoc';
 import {Typography} from 'antd';
-
+import * as S from './styled';
 const OpenapiDoc = () => {
   return (
-    <div>
+    <S.Container>
       <rapi-doc
         spec-url="https://api.apis.guru/v2/specs/abstractapi.com/geolocation/1.0.0/openapi.yaml"
         render-style="read"
@@ -14,16 +14,23 @@ const OpenapiDoc = () => {
         font-size="large"
         primary-color="#3b82f6"
         theme="light"
-        bg-color="#F4F4F5"
+        bg-color="#fff"
+        nav-bg-color="#fff"
+        use-path-in-nav-bar="true"
+        nav-active-item-marker="display:none;"
         regular-font="roboto"
+        layout="column"
+        schema-style="tree"
+        show-header="false"
+        show-method-in-nav-bar="as-colored-block"
         style={{height: '100vh', width: '100%'}}
       >
-        <div>
-          <Typography.Title>Developer Portal</Typography.Title>
+        <div slot="nav-logo" style={{width: '100%', height: 56, display: 'flex', alignItems: 'center', gap: 16}}>
+          <img src={KuskLogo} style={{width: 30, height: 30, verticalAlign: 'top'}} />
+          <h2 style={{marginBottom: 0}}>Kusk</h2>
         </div>
-        <img slot="logo" src={KuskLogo} style={{width: 30, height: 30}} />
       </rapi-doc>
-    </div>
+    </S.Container>
   );
 };
 export default OpenapiDoc;
---
+++ src/components/OpenapiDoc/styled.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  rapi-doc::part(section-navbar) {
+    border-right: 1px solid #e2e8f0;
+  }
+
+  rapi-doc::part(section-tag) {
+    border-top: 1px solid #e2e8f0;
+  }
+
+  rapi-doc::part(label-selected-server) {
+    display: none;
+  }
+`;

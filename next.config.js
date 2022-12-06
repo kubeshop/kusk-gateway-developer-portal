a9cca35c9376a1e72df9fecdcb872acbaa7b955e
Mahmoud Mortada <mortda.m.11@gmail.com>
2022-12-06 12:41:55 +0200

    fix: add roboto font

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
 	experimental: {
 		appDir: true,
+		fontLoaders: [
+			{
+				loader: "@next/font/google",
+				weight: ["400", "500", "700", "900"],
+				subsets: ["latin"],
+				preload: true,
+			},
+		],
 	},
 	assetPrefix: process.env.ASSET_URL || undefined,
 	env: {
